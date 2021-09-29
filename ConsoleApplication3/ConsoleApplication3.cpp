963ec7cc77298d9e5d5a816b236d6cc5f0b20aee
Swarga <xx26@bk.ru>
2021-09-29 14:19:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+//#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    long long pow = 1;
+    int count = 0;
+
+    for (int i = 0; i <= 63; i++) {
+        if (n & pow) count++;
+        pow *= 2;
+    }
+    cout << count;
+}
+
